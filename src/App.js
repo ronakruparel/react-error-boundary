ffacf8ac9adbec717c69fcb1002158d889903e83
ronakruparel <rr9323@gmail.com>
2019-03-03 23:19:10 +0530

    feat(app):first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class ErrorWrap extends Component {
+    state = {
+        error: null,
+        errorInfo: null
+    };
+    componentDidCatch(error, errorInfo) {
+        this.setState({ error: error, errorInfo: errorInfo });
+    }
+
+    render() {
+        if (this.state.errorInfo) {
+            return <h1>Oops something went wrong</h1>;
+        }
+        return this.props.children;
+    }
 }
 
+class Counter extends Component {
+    state = {
+        count: 0,
+        hasError: false
+    };
+
+    handleClick = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    };
+
+    render() {
+        if (this.state.count === 5) {
+            throw new Error("I crashed");
+        }
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>{this.state.count}</p>
+                    <button className="app-button" onClick={this.handleClick}>
+                        Click 5 times to generate an Error
+                    </button>
+                </header>
+            </div>
+        );
+    }
+}
+function App() {
+    return (
+        <div>
+            <ErrorWrap>
+                <p>Sample Error boundary</p>
+                <Counter />
+            </ErrorWrap>
+        </div>
+    );
+}
 export default App;
